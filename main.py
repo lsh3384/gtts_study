3f3d6254670b578e026648b1d014390a97daabc5
lee <lsh3384@gmail.com>
2022-11-08 23:41:51 +0900

    modified to get txt content from txt file

--- main.py
+++ main.py
@@ -1,14 +1,19 @@
 from gtts import gTTS
 __author__ = 'info-lab'
 
-tts = gTTS(
-    text='안녕하세요',
-    lang='ko', slow=False
-)
-tts.save('ex_ko.mp3')
+with open("example.txt", "r", encoding='UTF8') as f:
+    txt = f.read()
+    # print(f.read())
+    print(txt)
 
-tts1 = gTTS(
-    text='Hello',
-    lang='en', slow=False
-)
-tts1.save('ex_en.mp3')
+    tts = gTTS(
+        text=str(txt),
+        lang='ko', slow=False
+    )
+    tts.save('ex_ko.mp3')
+
+    tts1 = gTTS(
+        text='Hello',
+        lang='en', slow=False
+    )
+    tts1.save('ex_en.mp3')
